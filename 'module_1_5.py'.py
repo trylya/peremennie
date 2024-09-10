4849bb2f2b1fe8b050780a22e719d74a9e44938b
Лена <trylya@mail.ru>
2024-09-10 22:36:20 +0300

    метод строк

---
+++ 'module_1_5.py'.py
@@ -0,0 +1,7 @@
+immutable_var = 1,2,3,4, True, 'sport'
+print('Immutable tuple:', immutable_var)
+# immutable_var[1]= 45
+
+mutable_list=[7,9,11,'pen', False]
+mutable_list=[5,3,1,'list', True]
+print('Mutable list:',mutable_list)
