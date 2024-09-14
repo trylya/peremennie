63e7ed5edef7d64e9095121d649656a8adcbfae3
Лена <trylya@mail.ru>
2024-09-14 09:31:33 +0300

    кортежи

--- 'module_1_5.py'.py
+++ 'module_1_5.py'.py
@@ -1,7 +1,9 @@
 immutable_var = 1,2,3,4, True, 'sport'
 print('Immutable tuple:', immutable_var)
-# immutable_var[1]= 45
+ #immutable_var[1]= 45,   File "C:\Users\Елена\PycharmProjects\pythonProject10\'module_1_5.py'.py", line 3
+    #immutable_var[1]= 45
+#IndentationError: unexpected indent
 
 mutable_list=[7,9,11,'pen', False]
-mutable_list=[5,3,1,'list', True]
+mutable_list[3]=275
 print('Mutable list:',mutable_list)
